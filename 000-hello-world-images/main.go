8861a8e6a85977c71aa3b8ffd44cf82f66c5fa2f
Waldemar Bira <waldemar.bira@olx.pl>
2020-07-20 17:49:11 +0200

    Added example with creating pods

--- 000-hello-world-images/main.go
+++ 000-hello-world-images/main.go
@@ -4,9 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	data := []byte("OK")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
 func main() {
+	http.HandleFunc("/healthy", healthCheck)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		userAgent := r.Header.Get("User-Agent")
 		log.Printf("%s - [%s %s %s] 200 [%s]", r.RemoteAddr, r.Method, r.URL.Path, r.Proto, userAgent)
